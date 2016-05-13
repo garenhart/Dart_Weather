ce09761b753def5abae8d9aa1c427226b602f965
Garen Hartunian <ghartunian@gmail.com>
2016-05-13 17:40:23 -0400

    Gradle upgrade

--- app/src/main/java/com/ghart/dartweather/DetailActivityFragment.java
+++ app/src/main/java/com/ghart/dartweather/DetailActivityFragment.java
@@ -14,9 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+
 public class DetailActivityFragment extends Fragment {
 
     private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
